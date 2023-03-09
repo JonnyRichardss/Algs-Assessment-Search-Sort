5f0ea362f0cb660e4f0d5ed9ead1d0fe0158bfa1
Jonny <unconfigured@null.spigotmc.org>
2023-03-09 09:55:59 +0000

    Added menu and file reading helper functions

--- Algs Assessment Search Sort/Helpers.cs
+++ Algs Assessment Search Sort/Helpers.cs
@@ -8,8 +8,51 @@ namespace Algs_Assessment_Search_Sort
 {
     internal static class Helpers
     {
-        //class of functions that I want to re-use in my code
+        //class of functions that perform miscellaneous tasks in my code
+        public static int intMenu(string heading, string[] options)//function that constructs a text menu with any number of options from options array
+        {
+            bool invalidSelection;
+            int output = 0;
+            string input = "";
+            //loop while there is no valid input
+            do
+            {
+                //display heading and iterate to show options
+                Console.WriteLine(heading);
+                for (int i = 1; i <= options.Length; i++)
+                {
+                    Console.WriteLine("{0}: {1}",i,options[i]);
+                }
+                Console.Write(">");
+                //take user input
+                input = Console.ReadLine() ??string.Empty; //the question mark thing is weird here but it makes the compiler happy
+                if (int.TryParse(input, out output)&& output>0 && output <=options.Length)  //if input is an int and in range of the menu
+                {
+                    invalidSelection = false;//continue
+                }
+                else
+                {
+                    //else loop
+                    invalidSelection = true;
+                    Console.WriteLine("Not a valid option!");
+                }
+            } while (invalidSelection);
 
+            return output;
+        }
+        public static int[] ReadFile(string path)
+        {
+            //read in file as string
+            string[] lines = File.ReadAllLines(path);
+            //convert to int
+            int[] output = new int[lines.Length];
+            int tempint;
+            for (int i=0;i<lines.Length; i++)
+            {
+                if (int.TryParse(lines[i],out tempint)) output[i] = tempint; //using this method as it is exception proof - not really necessary as known data will always be used
+            }
 
+            return output;
+        }
     }
 }
